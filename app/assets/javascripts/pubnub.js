f144dbdbb354185d7288196e718272f09b2f17ac
soutchay <chungvus@gmail.com>
2014-11-19 09:21:23 -0800

    saving changes to html

--- app/assets/javascripts/pubnub.js
+++ app/assets/javascripts/pubnub.js
@@ -1,4 +1,4 @@
-var PUBNUB_demo = PUBNUB.init({
-publish_key: 'pub-c-28c48402-cc66-488a-bb47-accd95f3900c',
-subscribe_key: 'sub-c-afabf17c-6a18-11e4-b944-02ee2ddab7fe'
-});
+// var PUBNUB_demo = PUBNUB.init({
+// publish_key: 'pub-c-28c48402-cc66-488a-bb47-accd95f3900c',
+// subscribe_key: 'sub-c-afabf17c-6a18-11e4-b944-02ee2ddab7fe'
+// });
